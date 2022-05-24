b87590588aaaebaedd24cc5a2e1ad0191b1af09f
caiowmp <contato.caiomp@gmail.com>
2022-05-24 15:15:57 -0300

    Tratando excessões e criando mensagens de erro e de sucesso

--- WebCrudCremeb/WebCrudCremeb/Controllers/UsuarioController.cs
+++ WebCrudCremeb/WebCrudCremeb/Controllers/UsuarioController.cs
@@ -36,32 +36,67 @@ namespace WebCrudCremeb.Controllers
 
         public IActionResult Apagar(int id)
         {
-            _usuarioRepositorio.Apagar(id);
-            return RedirectToAction("Index");
+            try
+            {
+                bool apagado = _usuarioRepositorio.Apagar(id);
+
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Usuario apagado com sucesso!";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = $"Erro ao apagar o usuário";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Erro ao apagar o usuário, tente novamente!\nErro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
 
         [HttpPost]
         public IActionResult Criar(UsuarioModel usuario)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _usuarioRepositorio.Adicionar(usuario);
+                if(ModelState.IsValid)
+            {
+                    _usuarioRepositorio.Adicionar(usuario);
+                    TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View(usuario);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Erro ao cadastrar o usuário, tente novamente!\nErro: {erro.Message}";
                 return RedirectToAction("Index");
             }
-
-            return View(usuario);
         }
 
         [HttpPost]
         public IActionResult Alterar(UsuarioModel usuario)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _usuarioRepositorio.Atualizar(usuario);
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.Atualizar(usuario);
+                    TempData["MensagemSucesso"] = "Usuário alterado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View("Editar",usuario);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Erro ao atualizar o usuário, tente novamente!\nErro: {erro.Message}";
                 return RedirectToAction("Index");
             }
-
-            return View("Editar",usuario);
         }
     }
 }
