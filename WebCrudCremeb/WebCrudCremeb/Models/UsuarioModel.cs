d8405568da9d1f7f6e3c42e57f983c6901914045
caiowmp <contato.caiomp@gmail.com>
2022-05-23 22:04:33 -0300

    Criando UsuarioModel

---
+++ WebCrudCremeb/WebCrudCremeb/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WebCrudCremeb.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+        public int Cep { get; set; }
+        public string Endereco { get; set; }
+        public bool Ativo { get; set; }
+        public string Documento_rg_pdf { get; set; }
+        public int Grupo_usuario_id { get; set; }
+    }
+}
