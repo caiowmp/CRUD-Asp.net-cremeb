a66bfe6ad7cb1a310217df00891011e4382ddb54
caiowmp <contato.caiomp@gmail.com>
2022-05-23 22:26:04 -0300

    Utilizando Entity Framework para fazer a integração com o abnco de dados utilizando code first

---
+++ WebCrudCremeb/WebCrudCremeb/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCrudCremeb.Models;
+
+namespace WebCrudCremeb.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
--- WebCrudCremeb/WebCrudCremeb/Program.cs
+++ WebCrudCremeb/WebCrudCremeb/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebCrudCremeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEntityFrameworkSqlServer()
+    .AddDbContext<BancoContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
 var app = builder.Build();
 
